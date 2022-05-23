5c969a62a781cf8220a09144fceb1e5dfe367763
fauxlty <flake_canopy0v@icloud.com>
2022-05-23 09:15:02 -0700

    New file based on John Kenney's PS commands

---
+++ Install-SCCMAdvertisedPatches.ps1
@@ -0,0 +1,3 @@
+$Application = (Get-WmiObject -Namespace "root\ccm\clientSDK" -Class CCM_SoftwareUpdate -ComputerName XXX.br.wal-mart.com | Where-Object { $_.EvaluationState -like "*0*" -or $_.EvaluationState -like "*1*"})
+
+ Invoke-WmiMethod -Class CCM_SoftwareUpdatesManager -Name InstallUpdates -ArgumentList (,$Application) -Namespace root\ccm\clientsdk -ComputerName XXX.br.wal-mart.com
