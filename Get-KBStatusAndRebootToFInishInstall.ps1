62995aa00a6e1b1b9ac4f326cec3a61f951b3811
fauxlty <flake_canopy0v@icloud.com>
2022-05-26 15:26:41 -0700

    New script to repo

---
+++ Get-KBStatusAndRebootToFInishInstall.ps1
@@ -0,0 +1,25 @@
+#Domain
+$ServerList = Get-Content -Path D:\SEID\Admins\JLC\Coding\PowerShell\patching-list-domaincontrollers.csv
+
+# Check Installed KB's vs KB Search List
+Write-Host "Checking $($ServerList.count) Servers"
+
+#$array=@()
+
+foreach ($Server in $ServerList | Select-Object) {
+    $ServerHotfix = get-hotfix -ComputerName $server | Where-Object {$_.HotfixID -Like "*KB5015019*"}
+    $installedon = $ServerHotfix.InstalledOn
+        If (
+
+        $null -eq $installedon
+
+        )
+
+{$installedon="Hotfix Not Installed On $server"}
+
+#{Restart-Computer -ComputerName $server -force
+#Start-Sleep -Seconds 180}
+
+$result = "$server,$installedon"
+$result
+}
