e72b8feb947e13db68092c2d55e3caea87cf9328
fauxlty <flake_canopy0v@icloud.com>
2022-06-15 09:23:37 -0700

    Updated to work without Intellisense issues and will display both the target and destination paths

--- Get-FilesToZipAndSend.ps1
+++ Get-FilesToZipAndSend.ps1
@@ -1,33 +1,71 @@
-#Based on "CopyFiles.ps1" by Melissa Cronquist, this will zip and ship a folder and path containing the various scripts and tools necessary to promote
-#a domain controller in our environment.
+<#
+.SYNOPSIS
+#==========================================================================
+ DCSSLInfo.PS1
+ Created by: james.carter@wal-mart.com
+ DATE  : 2022-06-15
 
-#Declare and clear variables
+ COMMENT: This Script will take a target directory, compress it using Windows' default compression via PS, and copy the newly-created ZIP file to remote servers using a user-defined target share and path.
+
+==========================================================================
+
+.Notes
+ VERSION HISTORY:
+   1.0 2022-06-15 - Initial release
+
+.Description
+ Create ZIP file using local target directory path, distribute to servers in "serverlist.txt", and decompress the file into the destination path.
+
+.EXAMPLE
+===============================================================================
+Select a local directory path to compress and distribute, and decompress the ZIP on each target server named in "serverlist.txt".
+
+    -Create your "serverlist.txt" and list the FQDN of the servers you need to distribute to.
+    -Define the source path, such as "D:\SEID\Scripts\DCPROMO\DomainControllerFiles" that needs to be compressed and distributed.
+    -Run script without arguments/options in the command line, as all variables are defined internally.
+
+===============================================================================
+#>
+
+
+$ZIPSourceDir = "D:\SEID\Scripts\DCPROMO\DomainControllerFiles"
+$ZIPFilePath = "D:\SEID\Admins\JLC\Coding\DCPROMO\dcpromo.zip"
+$DestSharePath = "H$\DCPROMO"
+$ZIPDestTargetPath = "\DomainControllerFiles"
+$ZIPDestDirectoryPath = $ZIPDestTargetPath
 $server = ""
 $servers = ""
 $ServersToScan = ""
-$direxists = ""
+[bool]$direxists = 0
 $present = 0
 $notpresent = 0
 
-#Import list of would-be domain controllers
+#Create ZIP archive of current files in the "DCPROMO\DomainControllerFiles" 
+#ZIPLocalSourceDir="D:\SEID\Scripts\DCPROMO\DomainControllerFiles"
+#$ZIPSourceFilePath="D:\SEID\Admins\JLC\Coding\DCPROMO\dcpromo.zip"
+#$DestSharePath="($.split('.')[0]+'$')"
+#$ZIPDestTargetPath="$Server\$DestSharePath"
+
+Compress-Archive -DestinationPath $ZIPFilePath -Force $ZIPSourceDir -CompressionLevel Fastest
+
 $servers = Get-Content "D:\SEID\Admins\JLC\Coding\PowerShell\serverlist.txt"
 $ServersToScan = $servers.Count
 
 foreach ($server in $servers) {
     Write-Host "Attempting $Server"
 
-    $direxists = Test-Path -Path \\$server\H$\DCPROMO -IsValid
-
-    if ($direxists = "True")
-    {
-        Copy-Item -Path D:\SEID\Admins\JLC\Coding\DCPROMO\dcpromo.zip -Recurse -destination \\$server\H$\DCPROMO -Force
-        Expand-Archive -Path \\$server\H$\DCPROMO\dcpromo.zip -DestinationPath \\$server\H$\DCPROMO -Force
+    $ZIPDestTargetPath = "\\$Server\$DestSharePath"
+    $direxists = (Test-Path -Path $ZIPDestDirectoryPath -IsValid)
+    write-host "Copying $ZIPFilePath to $ZIPDestTargetPath"
+    if ($direxists = $true) {
+        Copy-Item -Path $ZIPFilePath -Recurse -destination $ZIPDestTargetPath -Force
+        Expand-Archive -Path "$ZIPDestTargetPath\dcpromo.zip" -DestinationPath $ZIPDestTargetPath -Force
         $present++
     } 
     else {
-        New-Item -ItemType "directory" -Path \\$server\H$\DCPROMO -Force
-        Copy-Item -Path D:\SEID\Admins\JLC\Coding\DCPROMO\dcpromo.zip -Recurse -destination \\$server\H$\DCPROMO -Force
-        Expand-Archive -Path \\$server\H$\DCPROMO\dcpromo.zip -DestinationPath \\$server\H$\DCPROMO -Force
+        New-Item -ItemType "directory" -Path $ZIPDestTargetPath -Force
+        Copy-Item -Path $ZIPFilePath -Recurse -destination $ZIPDestTargetPath -Force
+        Expand-Archive -Path "$ZIPDestTargetPath\dcpromo.zip" -DestinationPath $ZIPDestTargetPath -Force
         $notpresent++
     }
 
@@ -36,3 +74,7 @@ foreach ($server in $servers) {
 Write-Host "Servers with directory: $present"
 Write-Host "Servers without directory: $notpresent"
 Write-Host "Total servers scanned and copied to: $ServersToScan"
+
+#Close up shop
+#$array=@()
+[gc]::Collect()
